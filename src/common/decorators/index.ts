de52f84ddb6e986970143e741b988d8ed65024aa
MKSOW <makhaly224@gmail.com>
2026-05-19 09:31:35 +0200

    feat: add CurrentUser decorator for accessing authenticated user in requests

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -2,3 +2,4 @@ export * from './param-id.decorator';
 export * from './paginate.decorator';
 export * from './roles.decorator';
 export * from './trim.decorator';
+export * from './current-user.decorator';
