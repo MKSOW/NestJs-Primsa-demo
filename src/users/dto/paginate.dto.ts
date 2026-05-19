75561fe012ac0ba564f16e3df21f98c03e7cbfd2
MKSOW <makhaly224@gmail.com>
2026-05-19 12:09:34 +0200

    feat: add role property to PaginateDto for role-based pagination

--- src/users/dto/paginate.dto.ts
+++ src/users/dto/paginate.dto.ts
@@ -1,5 +1,6 @@
 import { IsInt, IsOptional, Min, Max } from 'class-validator';
 import { Type } from 'class-transformer';
+import { Role } from '../../common/pipes/parse-role.pipe';
 
 export class PaginateDto {
   @IsOptional()
@@ -14,4 +15,6 @@ export class PaginateDto {
   @IsInt({ message: 'offset doit être un entier' })
   @Min(0, { message: 'offset doit être positif ou zéro' })
   offset?: number = 0;
+
+  role?: Role;
 }
